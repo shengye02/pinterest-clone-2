df45e5ee903f37719744f70e9aef62f5e1f084b0
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2020-12-21 17:03:42 -0800

    half done interests thing

--- src/components/Mainboard.js
+++ src/components/Mainboard.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Mainboard.css";
 import "./Pin.css";
 import Pin from "./Pin";
+import AddIcon from '@material-ui/icons/Add';
 
 const Mainboard = ({ interests, pins, getBoards, boardsToPick }) => {
   console.log(interests)
@@ -9,15 +10,24 @@ const Mainboard = ({ interests, pins, getBoards, boardsToPick }) => {
   return (
     <div className="app__body">
       <div className="mainboard">
-
-        <div className="interests">
-          <h2>Ideas in your feed are based on these topics</h2>
-          <div className="interests-container">
-            {
-              interests.map(interest => (
-                <img src={interest.img} alt={interest.name} />
-              ))
-            }
+        <div className="interests-container">
+          <div className="interests">
+            <h2>Ideas in your feed are based on these topics</h2>
+            <div className="interest-items-container">
+              {
+                interests.map(interest => (
+                  <div className="interest-card" style={{ background: `url('${interest.img}')` }}>
+                    <div className="placeholder"></div>
+                    <p>{interest.name}</p>
+                  </div>
+                ))
+              }
+              <div className="interest-selector-button interest-card">
+                <div className="plus-icon-container">
+                    <AddIcon />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
